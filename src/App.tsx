218b936010d29aa106e49b4b8551e8ccd663cf4e
Kate Shepel <MariyaKaterina@gmail.com>
2025-03-20 16:00:04 +0200

    add changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,30 @@ import { peopleFromServer } from './data/people';
 import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const [focusFlag, setFocusFlag] = useState(false)
+  const [isDropdownActive, setIsDropdownActive] = useState(false);
   const [query, setQuery] = useState('');
   const [appliedQuery, setAppliedQuery] = useState('');
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
 
   const applyQuery = useCallback(debounce(setAppliedQuery, 300), []);
 
-  const filteredPersons = peopleFromServer.filter(
-    person => person.name.toLowerCase().includes(appliedQuery.toLowerCase())
+  const filteredPersons = peopleFromServer.filter(person =>
+    person.name.toLowerCase().includes(appliedQuery.toLowerCase()),
   );
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
-        {selectedPerson ? <h1 className="title" data-cy="title">
-          {selectedPerson.name} ({selectedPerson.born} - {selectedPerson.died})
-        </h1> :
-          <h1 className='title' data-cy='title'>No selected person</h1>
-        }
+        {selectedPerson ? (
+          <h1 className="title" data-cy="title">
+            {selectedPerson.name} ({selectedPerson.born} - {selectedPerson.died}
+            )
+          </h1>
+        ) : (
+          <h1 className="title" data-cy="title">
+            No selected person
+          </h1>
+        )}
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
@@ -34,44 +39,50 @@ export const App: React.FC = () => {
               className="input"
               data-cy="search-input"
               value={query}
-              onChange={e => {
-                setQuery(e.target.value);
-                applyQuery(e.target.value);
+              onChange={event => {
+                setQuery(event.target.value);
+                applyQuery(event.target.value);
                 setSelectedPerson(null);
               }}
-              onFocus={() => setFocusFlag(true)}
+              onFocus={() => setIsDropdownActive(true)}
             />
           </div>
 
-          {focusFlag && <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              {filteredPersons.map(person => (
-                <div
-                  key={person.slug}
-                  className="dropdown-item"
-                  data-cy="suggestion-item"
-                  onClick={() => {
-                    setSelectedPerson(person);
-                    setQuery(person.name);
-                    setFocusFlag(false);
-                  }}
-                >
-                  <p
-                    className={cn({
-                      "has-text-link": person.sex === 'm',
-                      "has-text-danger": person.sex === 'f',
-                    })}
+          {isDropdownActive && (
+            <div
+              className="dropdown-menu"
+              role="menu"
+              data-cy="suggestions-list"
+            >
+              <div className="dropdown-content">
+                {filteredPersons.map(person => (
+                  <div
+                    key={person.slug}
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    onClick={() => {
+                      setSelectedPerson(person);
+                      setQuery(person.name);
+                      setIsDropdownActive(false);
+                    }}
                   >
-                    {person.name}
-                  </p>
-                </div>
-              ))}
+                    <p
+                      className={cn({
+                        'has-text-link': person.sex === 'm',
+                        'has-text-danger': person.sex === 'f',
+                      })}
+                    >
+                      {person.name}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-          </div>}
+          )}
         </div>
 
-        {
-          !filteredPersons.length && <div
+        {!filteredPersons.length && (
+          <div
             className="
             notification
             is-danger
@@ -84,7 +95,7 @@ export const App: React.FC = () => {
           >
             <p className="has-text-danger">No matching suggestions</p>
           </div>
-        }
+        )}
       </main>
     </div>
   );
